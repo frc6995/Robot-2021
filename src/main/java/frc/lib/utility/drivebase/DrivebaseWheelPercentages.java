d0049893813e76a6b1efddba9d1d5df3d8a5dbaa
JoeyFabel <joseph.fabel@gmail.com>
2021-01-23 10:22:02 -0800

    Cleaned and added javadoc to DrivebaseWheelPercentages

--- src/main/java/frc/lib/utility/drivebase/DrivebaseWheelPercentages.java
+++ src/main/java/frc/lib/utility/drivebase/DrivebaseWheelPercentages.java
@@ -16,27 +16,49 @@ public class DrivebaseWheelPercentages {
     double leftPercentage;
     double rightPercentage;
 
+    /**
+     * Set the left side wheel percentage to the given value.
+     * @param left The percentage
+     * @return This {@link #DrivebaseWheelPercentages()}
+     */
     public DrivebaseWheelPercentages setLeftPercentage(double left) {
-        this.leftPercentage = left;
+        leftPercentage = left;
         return this;
     }
 
+    /**
+     * Set the right side wheel percentage to the given value.
+     * @param right The percentage
+     * @return This {@link #DrivebaseWheelPercentages()}
+     */
     public DrivebaseWheelPercentages setRightPercentage(double right) {
-        this.rightPercentage = right;
+        rightPercentage = right;
         return this;
     }
 
+    /**
+     * Get the left side wheel percentage.
+     * @return the left wheel percentage
+     */
     public double getLeftPercentage(){
         return leftPercentage;
     }
 
+    /**
+     * Get the right side wheel percentage.
+     * @return the right wheel percentage
+     */
     public double getRightPercentage(){
         return rightPercentage;
     }
 
+    /**
+     * Clamp both sides' wheel percentages between -100% and 100%.
+     * @return this {@link #DrivebaseWheelPercentages()}
+     */
     public DrivebaseWheelPercentages clamp(){
-        this.leftPercentage = MathUtil.clamp(this.leftPercentage, -1, 1);
-        this.rightPercentage = MathUtil.clamp(this.rightPercentage, -1, 1);
+        leftPercentage = MathUtil.clamp(leftPercentage, -1, 1);
+        rightPercentage = MathUtil.clamp(rightPercentage, -1, 1);
         return this;
     }
 
