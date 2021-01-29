f35e9eb56006422abe4a224f1ddcf2d812e00b4e
Nosoup831 <nckim8312@gmail.com>
2021-01-29 14:39:35 -0800

    added constants file

--- src/main/java/frc/robot/subsystems/AgitatorS.java
+++ src/main/java/frc/robot/subsystems/AgitatorS.java
@@ -3,15 +3,19 @@ package frc.robot.subsystems;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.lib.wrappers.motorcontrollers.NomadTalonSRX;
 import frc.lib.wrappers.motorcontrollers.NomadVictorSPX;
+import frc.robot.constants.AgitatorConstantsKRen;
 
 public class AgitatorS extends SubsystemBase {
   private NomadTalonSRX leftMotor;
   private NomadVictorSPX rightMotor;
+  private AgitatorConstantsKRen constants;
   
   /** Creates a new AgitatorS.*/
   public AgitatorS() {
-    leftMotor = new NomadTalonSRX(31);
-    rightMotor = new NomadVictorSPX(32);
+    constants = new AgitatorConstantsKRen();
+
+    leftMotor = new NomadTalonSRX(constants.getLeftMotorID());
+    rightMotor = new NomadVictorSPX(constants.getRightMotorID());
   }
   
   @Override
