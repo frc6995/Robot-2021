bd442d2b756c4de7869fbc2df0984dc3ebcc652c
JoeyFabel <joseph.fabel@gmail.com>
2021-02-12 14:48:37 -0800

    Added some methods to ShooterS

--- src/main/java/frc/robot/subsystems/shooter/ShooterS.java
+++ src/main/java/frc/robot/subsystems/shooter/ShooterS.java
@@ -5,6 +5,7 @@ import edu.wpi.first.wpilibj.Servo;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.lib.wrappers.motorcontrollers.NomadSparkMax;
 import frc.robot.constants.SuperShooterSConstants;
+import frc.robot.subsystems.shooter.Turret.TurretRequestedStates;
 
 /** The Super Shooter Subsytem, it includes the {@link Shooter}, the {@link Hood}, and the {@link Turret}.
  * 
@@ -28,6 +29,48 @@ public class ShooterS extends SubsystemBase {
 
   @Override
   public void periodic() {
-    // This method will be called once per scheduler run
+    turret.periodic();
+  }
+
+  /**
+   * Move the {@linkn Hood} to the specified position.
+   * 
+   * @param position The desired position
+   */
+  public void moveHoodToPosition(double position){
+    hood.moveHoodToPosition(position);
+  }
+
+  /**
+   * Have the {@link Shooter} PID to the target speed.
+   * @param speed The desired turret speed, between -0.8 and 0.8
+   */
+  public void pidShooterToTargetSpeed(double speed){
+    shooter.pidToTargetSpeed(speed);
+  }
+
+  /**
+   * Run PID on the Spark Max to the specified setpoint.
+   */  
+  public void runTurretPID(){
+    turret.runPID();
+  }
+
+  /**
+   * Run PID to the setpoint, using motion magic. Currently Incomplete (commented out lines need to be added)
+   */
+  public void runTurretPIDWithMotionMagic(){
+    turret.runPIDWithMotionMagic();
+  }
+
+  /**
+   * Request the Turret to go to into the specified state with a target setpoint.
+   * If {@link TurretRequestedStates}.None is requested, the internal state is not changed.
+   * 
+   * @param state The {@link TurretRequestedStates} that you want the turret to enter
+   * @param setpoint The desired position for the turret
+   */
+  public void requestTurretState(TurretRequestedStates state, double setpoint){
+    turret.requestState(state, setpoint);
   }
 }
