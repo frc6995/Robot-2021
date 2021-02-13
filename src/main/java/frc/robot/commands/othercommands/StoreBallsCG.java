08976b0540129fc660f3d1d3327414e53c38303c
Nosoup831 <nckim8312@gmail.com>
2021-02-12 17:21:08 -0800

    Organized folders

--- src/main/java/frc/robot/commands/othercommands/StoreBallsCG.java
+++ src/main/java/frc/robot/commands/othercommands/StoreBallsCG.java
@@ -2,9 +2,10 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands;
+package frc.robot.commands.othercommands;
 
 import edu.wpi.first.wpilibj2.command.ParallelCommandGroup;
+import frc.robot.commands.intakecommands.IntakeSpinRetractedC;
 import frc.robot.subsystems.AgitatorS;
 import frc.robot.subsystems.ColumnS;
 import frc.robot.subsystems.IntakeS;
