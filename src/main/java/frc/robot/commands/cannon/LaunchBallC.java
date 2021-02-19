702ccd9f8b146cc8a278ec0e06403c71d7a1358f
JoeyFabel <joseph.fabel@gmail.com>
2021-02-19 14:09:49 -0800

    Moved LaunchBallC into the commands folder.

--- src/main/java/frc/robot/commands/cannon/LaunchBallC.java
+++ src/main/java/frc/robot/commands/cannon/LaunchBallC.java
@@ -2,10 +2,11 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.subsystems.cannon;
+package frc.robot.commands.cannon;
 
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import frc.robot.subsystems.ColumnS;
+import frc.robot.subsystems.cannon.CannonS;
 
 public class LaunchBallC extends CommandBase {
   private CannonS cannon;
