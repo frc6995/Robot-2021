64a5892ba9acd477b459367660df760b819077e5
BroDudeGuyMan <hotsauce365247@gmail.com>
2021-10-16 14:48:40 -0800

    Added TurretHomeC to the ExtendClimberCG
    Signed-off-by: BroDudeGuyMan <hotsauce365247@gmail.com>

--- src/main/java/frc/robot/commands/climber/ExtendClimberCG.java
+++ src/main/java/frc/robot/commands/climber/ExtendClimberCG.java
@@ -3,10 +3,14 @@ package frc.robot.commands.climber;
 import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
 import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.subsystems.ClimberS;
+import frc.robot.subsystems.cannon.CannonS;
+import frc.robot.commands.cannon.*;
 
 public class ExtendClimberCG extends SequentialCommandGroup {
+
 	/** Creates a new ExtendClimberCG. */
-	public ExtendClimberCG(ClimberS climber) {
-		addCommands(new DeployClimberC(climber), new WaitCommand(1), new DisengageRatchetC(climber));
+	public ExtendClimberCG(ClimberS climber, CannonS cannon) {
+		addCommands(new TurretHomeC(cannon), new DeployClimberC(climber), new WaitCommand(1),
+				new DisengageRatchetC(climber));
 	}
 }
