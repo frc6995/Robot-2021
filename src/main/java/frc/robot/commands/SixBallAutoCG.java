6eff29cf655279a8615f34915661b0125e59069a
sammcdo <sam_mcdo@yahoo.com>
2021-10-30 11:46:36 -0700

    Testing Auto
    Signed-off-by: sammcdo <sam_mcdo@yahoo.com>

--- src/main/java/frc/robot/commands/SixBallAutoCG.java
+++ src/main/java/frc/robot/commands/SixBallAutoCG.java
@@ -7,6 +7,7 @@ import edu.wpi.first.wpilibj2.command.WaitCommand;
 import frc.robot.commands.cannon.SpinUpAndAimC;
 import frc.robot.commands.column.ColumnFeedC;
 import frc.robot.commands.drivebase.DriveAutoC;
+import frc.robot.commands.drivebase.DriveAutoCurvatureC;
 import frc.robot.commands.intake.IntakeSpinWhileHeldC;
 import frc.robot.commands.othercommands.AgitatorSpinC;
 import frc.robot.subsystems.AgitatorS;
@@ -19,15 +20,16 @@ import frc.robot.subsystems.cannon.CannonS;
 public class SixBallAutoCG extends ParallelCommandGroup {
   /** Creates a new SixBallAutoCG. */
   public SixBallAutoCG(CannonS cannon, LimelightS limelight, IntakeS intake, AgitatorS agitator, ColumnS column, DrivebaseS drivebase) {
-    addCommands(new SpinUpAndAimC(cannon, limelight, true),
-    new IntakeSpinWhileHeldC(intake),
+    addCommands(/*new SpinUpAndAimC(cannon, limelight, true),*/
+    /*new IntakeSpinWhileHeldC(intake),*/
     new AgitatorSpinC(agitator),
     new SequentialCommandGroup(
         new WaitCommand(2.5),
       new InstantCommand(()->column.disableStopper(), column),
       new WaitCommand(.5),
       new ColumnFeedC(column, 0.85, 0.25, false)),
-    new DriveAutoC(drivebase, 4, true, 0.15).withTimeout(4)
+    new DriveAutoC(drivebase, 2, true, 0.15).withTimeout(2)
+    //new DriveAutoCurvatureC(drivebase, 2, true, 0.15, 0.15).withTimeout(2)
       );
   }
 }
